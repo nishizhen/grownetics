f3dbea5fca0acc15f8988373100c0e4f1b3a1f53
Nick Busey <nickabusey@gmail.com>
2019-07-19 10:41:43 -0600

    Disable organizations when ONSITE is true

--- Server/html/src/Model/Behavior/OrganizationBehavior.php
+++ Server/html/src/Model/Behavior/OrganizationBehavior.php
@@ -17,36 +17,48 @@ class OrganizationBehavior extends Behavior
 
     public function beforeSave($event, $entity, $options)
     {
-        $session = new Session();
-
-        # Only auto-add owner information for logged in users
-        if ($session->read('Auth.User')) {
-            if ($session->read('Config.organization_id')) {
-                $entity->owner_type = $this->_TYPES["Organization"];
-                $entity->owner_id = $session->read('Config.organization_id');
-            } else if ($session->read('Auth.User.id')) {
-                $entity->owner_type = $this->_TYPES["User"];
-                $entity->owner_id = $session->read('Auth.User.id');
+        dd($entity);
+        # Only perform Organization separation if we are not Onsite
+        if (!env('ONSITE')) {
+            $session = new Session();
+            // dd($this->getRequest()->getSession()->read('User.username'));
+            dd($session->read('Auth.User'));
+            # Only auto-add owner information for logged in users
+            if ($session->read('Auth.User')) {
+                dd("Got user");
+                if ($session->read('Config.organization_id')) {
+                    dd("Got org");
+                    $entity->owner_type = $this->_TYPES["Organization"];
+                    $entity->owner_id = $session->read('Config.organization_id');
+                } else if ($session->read('Auth.User.id')) {
+                    $entity->owner_type = $this->_TYPES["User"];
+                    $entity->owner_id = $session->read('Auth.User.id');
+                }
+            } else {
+                dd("No user");
             }
         }
     }
 
     public function beforeFind($event, $query, $options, $primary)
     {
-        $session = new Session();
+        # Only perform Organization separation if we are not Onsite
+        if (!env('ONSITE')) {
+            $session = new Session();
 
-        # Only auto-add owner information for logged in users
-        if ($session->read('Auth.User')) {
-            if ($session->read('Config.organization_id')) {
-                $query->where([
-                    'owner_type' => $this->_TYPES["Organization"],
-                    'owner_id' => $session->read('Config.organization_id')
-                ]);
-            } else if ($session->read('Auth.User.id')) {
-                $query->where([
-                    'owner_type' => $this->_TYPES["User"],
-                    'owner_id' => $session->read('Auth.User')->id
-                ]);
+            # Only auto-add owner information for logged in users
+            if ($session->read('Auth.User')) {
+                if ($session->read('Config.organization_id')) {
+                    $query->where([
+                        'owner_type' => $this->_TYPES["Organization"],
+                        'owner_id' => $session->read('Config.organization_id')
+                    ]);
+                } else if ($session->read('Auth.User.id')) {
+                    $query->where([
+                        'owner_type' => $this->_TYPES["User"],
+                        'owner_id' => $session->read('Auth.User')->id
+                    ]);
+                }
             }
         }
     }

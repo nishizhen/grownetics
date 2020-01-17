4dd90ec2d7732220d8ccfedc9d2acebf9a20639c
Nick Busey <nickabusey@gmail.com>
2020-01-17 17:30:34 +0000

    Resolve "Fix Review Apps"

--- Server/html/src/Model/Behavior/OrganizationBehavior.php
+++ Server/html/src/Model/Behavior/OrganizationBehavior.php
@@ -17,43 +17,45 @@ class OrganizationBehavior extends Behavior
 
     public function beforeSave($event, $entity, $options)
     {
+        return true;
         # Only perform Organization separation if we are not Onsite
-        if (!env('ONSITE') && !$entity->owner_id) {
-            $session = new Session();
+        // if (!env('ONSITE') && !$entity->owner_id) {
+        //     $session = new Session();
 
-            # Only auto-add owner information for logged in users
-            if (isset($options['_footprint'])) {
-                if ($session->read('Config.organization_id')) {
-                    $entity->owner_type = $this->_TYPES["Organization"];
-                    $entity->owner_id = $session->read('Config.organization_id');
-                } else if ($options['_footprint']['id']) {
-                    $entity->owner_type = $this->_TYPES["User"];
-                    $entity->owner_id = $options['_footprint']['id'];
-                }
-            }
-        }
+        //     # Only auto-add owner information for logged in users
+        //     if (isset($options['_footprint'])) {
+        //         if ($session->read('Config.organization_id')) {
+        //             $entity->owner_type = $this->_TYPES["Organization"];
+        //             $entity->owner_id = $session->read('Config.organization_id');
+        //         } else if ($options['_footprint']['id']) {
+        //             $entity->owner_type = $this->_TYPES["User"];
+        //             $entity->owner_id = $options['_footprint']['id'];
+        //         }
+        //     }
+        // }
     }
 
     public function beforeFind($event, $query, $options, $primary)
     {
+        return true;
         # Only perform Organization separation if we are not Onsite
-        if (!env('ONSITE')) {
-            $session = new Session();
+        // if (!env('ONSITE')) {
+        //     $session = new Session();
 
-            # Only auto-add owner information for logged in users
-            if (isset($options['_footprint'])) {
-                if ($session->read('Config.organization_id')) {
-                    $query->where([
-                        'owner_type' => $this->_TYPES["Organization"],
-                        'owner_id' => $session->read('Config.organization_id')
-                    ]);
-                } else if ($options['_footprint']['id']) {
-                    $query->where([
-                        'owner_type' => $this->_TYPES["User"],
-                        'owner_id' => $options['_footprint']['id']
-                    ]);
-                }
-            }
-        }
+        //     # Only auto-add owner information for logged in users
+        //     if (isset($options['_footprint'])) {
+        //         if ($session->read('Config.organization_id')) {
+        //             $query->where([
+        //                 'owner_type' => $this->_TYPES["Organization"],
+        //                 'owner_id' => $session->read('Config.organization_id')
+        //             ]);
+        //         } else if ($options['_footprint']['id']) {
+        //             $query->where([
+        //                 'owner_type' => $this->_TYPES["User"],
+        //                 'owner_id' => $options['_footprint']['id']
+        //             ]);
+        //         }
+        //     }
+        // }
     }
 }

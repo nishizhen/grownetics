571f3fc554965430054e8335b39cc79e2146fc7b
Nick Busey <nickabusey@gmail.com>
2020-07-08 12:46:39 -0600

    Migrate to REMOTE_DOMAIN

--- Server/html/src/Lib/ArgusApi.php
+++ Server/html/src/Lib/ArgusApi.php
@@ -19,7 +19,7 @@ class ArgusApi {
         $username = "APIuser";
         $password = "API123";
         # ARGUS_URL example: localhost:5555 or 192.168.88.75:47840
-        $remote_url = 'https://' . env('ARGUS_URL') . '/argusapi/v1/parameters/' . $parameterId . $requestParams;
+        $remote_domain = 'https://' . env('ARGUS_URL') . '/argusapi/v1/parameters/' . $parameterId . $requestParams;
 
         $opts = array(
             'http'=>array(
@@ -35,7 +35,7 @@ class ArgusApi {
         $context = stream_context_create($opts);
 
 
-        $file = file_get_contents($remote_url, false, $context);
+        $file = file_get_contents($remote_domain, false, $context);
 
         $response = json_decode($file);
         return $response;
